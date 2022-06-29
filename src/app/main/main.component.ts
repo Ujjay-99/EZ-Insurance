b29c3dc0a0097e23f46a0075fa6d85b37ab04aef
Suyash Wagh <suyashwagh22@gmail.com>
2022-06-30 01:48:26 +0530

    added-type-images-on-main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { AdminDataService } from '../admin/admin-services/admin-data.service';
 import { IInsuraceScheme } from '../models/iinsurace-scheme';
 import { IInsuranceType } from '../models/IInsuranceType';
+import { ISchemeWithImage } from '../models/ISchemeWithImage';
 
 @Component({
   selector: 'app-main',
@@ -11,18 +13,12 @@ import { IInsuranceType } from '../models/IInsuranceType';
 })
 export class MainComponent implements OnInit {
 
-  typeList:IInsuranceType[]=[];
-  schemesList:IInsuraceScheme[]=[];
-  
-  constructor(private dataService:AdminDataService,private router: Router) { 
-    this.dataService.viewType().subscribe(type=>{
-      console.log(type);
-      this.typeList=type;
-    })
-    this.dataService.viewScheme().subscribe(type=>{
-      console.log(type);
-      this.schemesList=type;
-    })
+  typeList:IInsuranceType[];
+  schemesList:ISchemeWithImage[];
+  imageUrl:any;
+  url:any;
+  constructor(private dataService:AdminDataService,private router: Router, private sanitizer: DomSanitizer) { 
+    
   }
   // planDetails(id:string){
   //   console.log(id);
@@ -35,7 +31,20 @@ export class MainComponent implements OnInit {
     
   }
   ngOnInit(): void {
-
+    this.dataService.viewType().subscribe(type=>{
+      console.log(type);
+      this.typeList=type;
+    })
+    this.dataService.viewSchemesWithImage().subscribe(type=>{
+      this.success(type);
+    })
   }
 
+  success(data:ISchemeWithImage[]){
+    this.schemesList = data;
+    for(let i = 0; i < this.schemesList.length; i++){
+      this.url = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64, ${this.schemesList[i].insranceTypeImage}`);
+      this.schemesList[i].url = this.url;
+    }
+  }
 }
